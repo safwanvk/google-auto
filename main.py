3df79966912fae5babe697e1da704f5f6a5757d3
safwanvk <safwanvalakundil@gmail.com>
2020-08-04 20:19:35 +0530

    :white_check_mark: Set fetch & input

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from selenium import webdriver
 browser = webdriver.Chrome(executable_path='/home/safwan/chromedriver_linux64/chromedriver')
 
 browser.get('https://www.google.com')
+
+search_input = browser.find_element_by_name('q')
+search_input.send_keys('python web development')
+
