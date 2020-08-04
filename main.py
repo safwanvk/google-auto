f7dcd8b4a2781708f4f4057dbedd0b51c012af4a
safwanvk <safwanvalakundil@gmail.com>
2020-08-04 20:31:39 +0530

    :white_check_mark: Set auto

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from selenium import webdriver
+import time
 
 browser = webdriver.Chrome(executable_path='/home/safwan/chromedriver_linux64/chromedriver')
 
 browser.get('https://www.google.com')
 
+time.sleep(2)
+
 search_input = browser.find_element_by_name('q')
 search_input.send_keys('python web development')
+
+time.sleep(2)
+
+search_btn = browser.find_element_by_css_selector('input[type="submit"]')
+search_btn.click()
 
